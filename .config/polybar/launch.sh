66a8a9b86b6ae8d3e11835df07957cb169f15cec
Kyle James Rhoads <rhoadskj@gmail.com>
2020-07-15 19:38:12 -0400

    changed mouse speed

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 killall -q polybar
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 >>/tmp/polybar1.log 2>&1 &
-polybar bar2 >>/tmp/polybar2.log 2>&1 &
+polybar example &
 echo "polybar launched"
